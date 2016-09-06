8c79167706be5366a337d75c4e0b96f3b0489eb3
kavita.patel-btc <kavita.patel@botreetechnologies.com>
2016-09-06 14:27:05 +0530

    add data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,9 @@ class ApplicationController < ActionController::Base
   protected
   def set_i18n_locale_from_params
     if params[:locale]
-      
       if I18n.available_locales.map(&:to_s).include?(params[:locale])
         I18n.locale = params[:locale]
       else
-
         flash.now[:notice] = 
         "#{params[:locale]} tarnslation not available"
         logger.error flash.now[:notice] 
