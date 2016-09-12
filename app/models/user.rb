cb6d7d34d6a9fadb331c31ded8080b3ef7fab6c5
kavita.patel-btc <kavita.patel@botreetechnologies.com>
2016-09-12 12:08:15 +0530

    add registration link and login link

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence:true, uniqueness: true
   has_secure_password
-  validates :password, :presence =>true, :confirmation => true, :length =>{:within =>6..40}
+  validates :password, :presence =>true, :confirmation => true, :length =>{:within =>2..40}
   after_destroy :ensure_an_admin_remains
 
   private
